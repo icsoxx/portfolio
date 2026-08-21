7bfff60aba62e5b18aec40bec778daa2f2f2431e
Rizqie Fahla Septiana <rizqie.fahla@gmail.com>
2026-08-21 15:42:07 +0700

    Add portfolio website
    Initial version of my personal Data Analyst portfolio website, featuring my projects, skills, certificates, and contact information.

---
+++ script.js
@@ -0,0 +1,22 @@
+
+document.querySelectorAll("a[href^='#']").forEach(link => {
+
+    link.addEventListener("click", function(event) {
+
+        const target = document.querySelector(
+            this.getAttribute("href")
+        );
+
+        if (target) {
+
+            event.preventDefault();
+
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+
+        }
+
+    });
+
+});
